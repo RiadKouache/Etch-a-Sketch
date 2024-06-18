8b3711cbc814b6c18157080775f4cc6bdcaf7efc
riadkche <141369946+RiadKouache@users.noreply.github.com>
2024-06-19 00:00:56 +0100

    envlopping the code into functions + adding events for clearing and resizing the grid

--- app.js
+++ app.js
@@ -1,53 +1,62 @@
-// const grid = document.querySelector(".grid");
+function createGrid(squaresNumber) {
+  const grid = document.querySelector(".grid");
 
-// const gridWidth = grid.getBoundingClientRect().width;
-// const gridHeight = grid.getBoundingClientRect().height;
+  let gridWidth = grid.getBoundingClientRect().width - 4;
+  let gridHeight = grid.getBoundingClientRect().height - 4;
+  let squareWidth = gridWidth / squaresNumber;
+  let squareHeight = gridHeight / squaresNumber;
 
-// const squareWidth = gridWidth / 16;
-// const squareHeight = gridHeight / 16;
+  const subheading = document.querySelector(".subheading");
+  subheading.textContent = squaresNumber + "x" + squaresNumber + " Grid";
 
-// const squareNumber = 16 * 16;
-// let sum = 0;
+  for (let i = 0; i < squaresNumber; i++) {
+    for (let j = 0; j < squaresNumber; j++) {
+      const squareDiv = document.createElement("div");
+      squareDiv.style.width = squareWidth + "px";
+      squareDiv.style.height = squareHeight + "px";
+      squareDiv.classList.add("square");
+      grid.appendChild(squareDiv);
 
-// for (let i = 0; i < 16; i++) {
-//   for (let j = 0; j < 16; j++) {
-//     const squareDiv = document.createElement("div");
-//     squareDiv.style.width = squareWidth + "px";
-//     squareDiv.style.height = squareHeight + "px";
-//     squareDiv.classList.add("square");
-//     grid.appendChild(squareDiv);
-//     sum++;
-//   }
-// }
+      // const square = document.querySelector(".square");
 
-// console.log(squareNumber, sum);
-
-const grid = document.querySelector(".grid");
-
-let gridWidth = grid.getBoundingClientRect().width - 4;
-let gridHeight = grid.getBoundingClientRect().height - 4;
-
-let squareWidth = gridWidth / 16;
-let squareHeight = gridHeight / 16;
-
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.style.width = squareWidth + "px";
-    squareDiv.style.height = squareHeight + "px";
-    squareDiv.classList.add("square");
-    grid.appendChild(squareDiv);
-
-    // const square = document.querySelector(".square");
-
-    squareDiv.addEventListener("mouseover", (e) => {
-      let rgb1 = Math.floor(Math.random() * 256);
-      let rgb2 = Math.floor(Math.random() * 256);
-      let rgb3 = Math.floor(Math.random() * 256);
-      if (!e.target.style.backgroundColor) {
-        e.target.style.backgroundColor =
-          "rgb(" + rgb1 + "," + rgb2 + "," + rgb3 + ")";
-      }
-    });
+      squareDiv.addEventListener("mouseover", (e) => {
+        let rgb1 = Math.floor(Math.random() * 256);
+        let rgb2 = Math.floor(Math.random() * 256);
+        let rgb3 = Math.floor(Math.random() * 256);
+        if (!e.target.style.backgroundColor) {
+          e.target.style.backgroundColor =
+            "rgb(" + rgb1 + "," + rgb2 + "," + rgb3 + ")";
+        }
+      });
+    }
   }
 }
+
+function sketchClear() {
+  document.querySelectorAll(".square").forEach((e) => e.remove());
+}
+
+//initialize grid
+defaultGrid = 16;
+createGrid(defaultGrid);
+
+//reset button
+let reset = document.querySelector(".reset");
+reset.addEventListener("click", () => {
+  sketchClear();
+  createGrid(defaultGrid);
+});
+
+//change grid with new size
+const changeButton = document.querySelector(".changeButton");
+changeButton.addEventListener("click", () => {
+  let squaresNumber = -1;
+  while (squaresNumber > 100 || squaresNumber < 0) {
+    squaresNumber = Number(
+      prompt("Enter a new number of squares per side (max=100):")
+    );
+  }
+  defaultGrid = squaresNumber;
+  sketchClear();
+  createGrid(squaresNumber);
+});
