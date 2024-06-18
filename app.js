24b5ba7a9dfbb7269e3a30d001d7c74caec28b35
riadkche <141369946+RiadKouache@users.noreply.github.com>
2024-06-18 21:09:41 +0100

    fixing the divs positionning bug: the error was in the calculs of the spaces with javascript, and turn the width and height variables from const to let

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
+// const grid = document.querySelector(".grid");
+
+// const gridWidth = grid.getBoundingClientRect().width;
+// const gridHeight = grid.getBoundingClientRect().height;
+
+// const squareWidth = gridWidth / 16;
+// const squareHeight = gridHeight / 16;
+
+// const squareNumber = 16 * 16;
+// let sum = 0;
+
+// for (let i = 0; i < 16; i++) {
+//   for (let j = 0; j < 16; j++) {
+//     const squareDiv = document.createElement("div");
+//     squareDiv.style.width = squareWidth + "px";
+//     squareDiv.style.height = squareHeight + "px";
+//     squareDiv.classList.add("square");
+//     grid.appendChild(squareDiv);
+//     sum++;
+//   }
+// }
+
+// console.log(squareNumber, sum);
+
 const grid = document.querySelector(".grid");
 
-const gridWidth = grid.getBoundingClientRect().width;
-const gridHeight = grid.getBoundingClientRect().height;
+let gridWidth = grid.getBoundingClientRect().width - 4;
+let gridHeight = grid.getBoundingClientRect().height - 4;
 
-const squareWidth = gridWidth / 16;
-const squareHeight = gridHeight / 16;
-
-const squareNumber = 16 * 16;
-let sum = 0;
+let squareWidth = gridWidth / 16;
+let squareHeight = gridHeight / 16;
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
@@ -16,8 +37,5 @@ for (let i = 0; i < 16; i++) {
     squareDiv.style.height = squareHeight + "px";
     squareDiv.classList.add("square");
     grid.appendChild(squareDiv);
-    sum++;
   }
 }
-
-console.log(squareNumber, sum);
