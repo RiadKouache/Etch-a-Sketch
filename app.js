16b5b7e8ab26a98eff718a9cc12949efbc1b6976
riadkche <141369946+RiadKouache@users.noreply.github.com>
2024-06-18 17:48:07 +0100

    this files are not fixed , there is issues with positionnement of the divs

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const grid = document.querySelector(".grid");
 
 const gridWidth = grid.getBoundingClientRect().width;
+const gridHeight = grid.getBoundingClientRect().height;
 
 const squareWidth = gridWidth / 16;
+const squareHeight = gridHeight / 16;
+
+const squareNumber = 16 * 16;
+let sum = 0;
 
 for (let i = 0; i < 16; i++) {
-  const squareDiv = document.createElement("div");
-  squareDiv.style.width = squareWidth + "px";
-  squareDiv.style.height = squareWidth + "px";
-  grid.appendChild(squareDiv);
+  for (let j = 0; j < 16; j++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.style.width = squareWidth + "px";
+    squareDiv.style.height = squareHeight + "px";
+    squareDiv.classList.add("square");
+    grid.appendChild(squareDiv);
+    sum++;
+  }
 }
+
+console.log(squareNumber, sum);
